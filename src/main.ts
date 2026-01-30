0d9f82719357a20c3a3c8ad2d0acc11266353683
DennisMajestie <majestydennis6@gmail.com>
2026-01-30 04:12:39 +0100

    chore: add environment logging to debug production

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    console.log('🚀 App Started');
+    console.log('🌍 Environment:', environment);
+    console.log('🔗 API URL:', environment.apiUrl);
+  })
   .catch((err) => console.error(err));
